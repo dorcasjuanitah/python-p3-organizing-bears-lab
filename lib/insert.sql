4c3d11db25b7cb8bd98660e104d83708b67849a1
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-06-05 10:11:18 +0300

    sql

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color) VALUES 
+('Mr.Chocolate', 10, 'M', 'Black'),
+('Rowdy', 15, 'F', 'Brown'),
+('Tabitha', 24, 'F', 'Black'),
+('Grinch', 50, 'M', 'White'),
+('Sergent Brown', 28, 'M', 'Black'),
+('Mellisa', 20, 'F', 'Brown'),
+('Wendy', 26, 'F', 'Black'),
+( NULL, 40, 'M', 'White');
